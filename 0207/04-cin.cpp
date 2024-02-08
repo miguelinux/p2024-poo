12e410b04ecada881804ef1e633680cd2e8d1a27
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2024-02-07 19:01:34 -0600

    ejemplo cin

---
+++ 0207/04-cin.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int numero;
+  cout << "Escribe un número: ";
+  cin >> numero;
+  cout << "El número que escribiste es: " << numero << endl;
+  return 0;
+}
