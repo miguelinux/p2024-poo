0840507f490c92206b178dc81b45d73bf0d89f41
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2024-02-07 18:20:15 -0600

    ejemplo con tabs

---
+++ 0207/ejemplo-02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hola, esto es un mensaje\n"
+  cout << "de la clase de C++.\n\n"
+  cout << "Aqui vemos un\ttabulador" << endl;
+  cout << "Aca vemos una diagonal \\" << endl;
+  cout << "Sirve para las rutas de Windows\n";
+  cout << "Como: C:\\Users\\NoSe\\otro\\directorio\n";
+  cout << "con tabs: C:\tUsers\tNoSe\totro\tdirectorio\n";
+  cout << "con commillas: C:\"Users\"NoSe\"otro\"directorio\n";
+  cout << "\n";
+  cout << endl;
+  cout "\t\t\tFin";
+  return 0;
+}
