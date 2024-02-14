05186e97eb94d62c028428b05d935a2b52b08ce7
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2024-02-14 17:38:58 -0600

    set presicion

--- 0214/01-flotantes.cpp
+++ 0214/01-flotantes.cpp
@@ -6,5 +6,9 @@ int main() {
   double d = 0.123456789;
   cout << "Float : " << f << endl;
   cout << "Double: " << d << endl;
+  cout << "Float : " << setpresicion(3) << f << endl;
+  cout << "Double: " << setpresicion(3) << d << endl;
+  cout << "Float : " << setpresicion(15) << f << endl;
+  cout << "Double: " << setpresicion(15) << d << endl;
   return 0;
 }
