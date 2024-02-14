eb779c67567799fbd7902cd9b9a7cc4504200dbd
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2024-02-14 17:27:32 -0600

    ejemplo de flotantes

---
+++ 0214/01-flotantes.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  float  f = 0.123456789;
+  double d = 0.123456789;
+  cout << "Float : " << f << endl;
+  cout << "Double: " << d << endl;
+  return 0;
+}
