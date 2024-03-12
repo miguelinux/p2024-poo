6b104b020ef9bb57ca0fd84416f024bb2349ce55
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2024-03-12 16:47:37 -0600

    ejemplos cadenas

---
+++ 0312/02-cadena_caracter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string txt = "Hola";
+  txt[0] = 'J';
+  cout << txt << endl;
+  return 0;
+}
---
+++ 0312/03-caracteres_de_escape.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string txt = "Hola,\n\"buenos dias\" a\ttodos\n\t\t\'todos\'";
+  cout << txt << endl;
+  txt = "C:\\Users\\Mi Usuario\\Documentos\\hola.cpp";
+  cout << txt << endl;
+  return 0;
+}
